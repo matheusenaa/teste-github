3f5c304ecec6d475f88f06357e176d63feeeb294
Matheus Sena <matheusenaa@gmail.com>
2020-05-12 11:05:19 -0300

    Projeto Criado

---
+++ src/teste/program.java
@@ -0,0 +1,12 @@
+
+package teste;
+
+
+public class program {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("teste.program.main()");
+    }
+    
+}
