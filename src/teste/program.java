fce441e980f6d01c53aa8e5b4c1d9efe5638d282
Matheus Sena <matheusenaa@gmail.com>
2020-05-14 14:55:06 -0300

    adc bom dia

--- src/teste/program.java
+++ src/teste/program.java
@@ -7,6 +7,7 @@ public class program {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("teste.program.main()");
+        System.out.println("bom dia");
     }
     
 }
